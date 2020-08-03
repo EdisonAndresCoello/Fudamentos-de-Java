28efd426fa7e92010bdff0d756af22dd8c3685bd
EdisonAndresCoello <68083035+EdisonAndresCoello@users.noreply.github.com>
2020-08-02 19:27:44 -0500

    Actualizacion

---
+++ Cuenta/src/CuentaAhorro.java
@@ -0,0 +1,13 @@
+public class CuentaAhorro extends CuentaBancaria {
+	public void depositar(double dinero) {
+		if(dinero>0 && dinero<=1000) {
+			dinero += (dinero*0.02);
+			saldo +=dinero;
+		}else{
+			saldo +=dinero;		
+		}
+}
+@Override
+public void retirar(double dinero) {	
+}
+}
