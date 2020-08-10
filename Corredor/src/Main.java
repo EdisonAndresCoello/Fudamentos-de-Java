209776ba0cb9cc5ce3c537fb6f0b583c6ac4a6eb
EdisonAndresCoello <68083035+EdisonAndresCoello@users.noreply.github.com>
2020-08-10 10:02:38 -0500

    Act.

--- Corredor/src/Main.java
+++ Corredor/src/Main.java
@@ -15,6 +15,12 @@ public class Main {
 	       corr2.verificarEnergia();
 	       corr2.entrenar();
 	       corr2.verificarEnergia();
+
+	       corr2.verificarEnergia();
+	       corr2.recargarEnergia(90);
+	       corr2.verificarEnergia();
+	       corr2.entrenar();
+	       corr2.verificarEnergia();
 	       
 	}
 	
